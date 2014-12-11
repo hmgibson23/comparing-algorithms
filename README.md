6f7897e350369416221e81ec1becd74e32d4e564
hmgibson23 <hugo.gibson@yahoo.co.uk>
2014-12-11 10:18:36 +0000

    Lesson outline and first lesson completed

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ comparing-algorithms
 ====================
 
 Course material for comparing algorithms class
+
+
+### Lesson 1 - Search and simple Recursion
+
+An introduction is how to figure out the complexity of a couple of
+search algorithms and some well-known recursive monsters.
+
+### Lesson 2 - Data Structures
+
+Differences between linked lists, binary trees, heaps and arrays.
+
+### Lesson 3 - Sorting
+
+Because it wouldn't be a course on algorithms without showing off some
+famous sorting algorithms.
+
+### Lesson 4 - Advanced (sort of) techniques
+
+Randomised algorithms and data structures and dynamic programming.
+
+When should you use a Skip List over a normal linked list? How does
+randomisation help optimisation? WTF is dynamic programming and how
+can it help you?
--- lesson-1/Search.py
+++ lesson-1/Search.py
@@ -1,9 +1,13 @@
-
+import sys
+sys.path.append("..")
+from helpers.Helpers import *
 
 # A linear search algorithm
-def findLinear(val, values):
+@timing
+def linearSearch(val, values):
     position = 0
     found = False
+
     while position < len(values) and not found:
         if values[position] == val:
             break;
@@ -12,14 +16,61 @@ def findLinear(val, values):
 
     return position
 
-# A binary search algortihm
+# A binary search algorithm
+@timing
+def binarySearch(val, values):
+    min = 0
+    max = len(values) - 1
+    while True:
+        if max < min:
+            return -1
+        m = (min + max) // 2
+        if values[m] < val:
+            min = m + 1
+        elif values[m] > val:
+            max = m - 1
+        else:
+            return m
+
+
+###### RECURSIVE ALGORITHMS ######
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+## As the above functions are recursive, these
+## helpers allows to time the whole call, not each
+## function call
+@timing
+def fnHelper(fn, n):
+    val = fn(n)
+    return val
+
+
+
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
 
 
 def main():
-    values = [1,2,3,4,5,6,7,8,9,10]
-    val = 6
-    pos = findLinear(val, values)
-    print "Found: " + str(val) + " at index: " + str(pos)
+    values = range(1, 1000000)
+    val = 340100
+    pos = linearSearch(val, values)
+    print "Linear search found: " + str(val) + " at index: " + str(pos) + "\n"
+
+    pos = binarySearch(val, values)
+    print "Binary search found: " + str(val) + " at index: " + str(pos) + "\n"
+
+    # fact = fnHelper(factorial, 5)
+    # print "factorial(" + str(5) + ") is " + str(fact) + "\n"
+    # fib = fnHelper(fibonacci, 5)
+    # print "fibonacci(" + str(5) + ") is " + str(fib) + "\n"
 
 if __name__ == "__main__":
     main()
